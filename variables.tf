3f5ece5e43b9c5567476ef643dc11754c923e7a3
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2025-11-25 23:32:21 +0100

    fix: add description to variable role_based_access_control

--- variables.tf
+++ variables.tf
@@ -568,7 +568,7 @@ variable "role_based_access_control" {
     managed            = bool
     tenant_id          = optional(string)
     azure_rbac_enabled = bool
-  description = "Configures role-based access control (RBAC) settings for the Azure Kubernetes Service (AKS) cluster. * managed: Specifies whether Azure manages the RBAC. * tenant_id: (Optional) The Tenant ID which should be used for authentication. * azure_rbac_enabled: Specifies whether Azure RBAC is enabled fo..."
+  description = "Configures role-based access control (RBAC) settings for the Azure Kubernetes Service (AKS) cluster. * managed: Whether Azure RBAC is managed by Azure AD. * tenant_id: (Optional) The Tenant ID which should be used for authentication. * azure_rbac_enabled: Whether to enable Azure RBAC for Kubernetes authorization."
   }))
   default = null
 }
