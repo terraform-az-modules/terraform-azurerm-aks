c1c8f2cf5b10c8014fae5603d3b874656b874c26
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2025-11-26 00:10:59 +0100

    fix: add output for aks.fqdn

--- outputs.tf
+++ outputs.tf
@@ -28,3 +28,8 @@ output "key_vault_secrets_provider" {
   description = "Specifies the obejct id of key vault secrets provider "
 }
 
+output "aks_fqdns" {
+  description = "Map of fully qualified domain name"
+  value       = { for k, v in azurerm_kubernetes_cluster.aks : k => v.fqdn }
+}
+
