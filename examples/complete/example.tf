1d15b55f3e7b4b024f6d94e76cd9e0ac2c4ad96c
Vedant <vedant.kalia@clouddrove.com>
2025-12-09 01:29:42 +0530

    fix: Updated Module and Updated Workflows

---
+++ examples/complete/example.tf
@@ -0,0 +1,163 @@
+##-----------------------------------------------------------------------------
+## Provider
+##-----------------------------------------------------------------------------
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current_client_config" {}
+
+##-----------------------------------------------------------------------------
+## Resource Group
+##-----------------------------------------------------------------------------
+module "resource_group" {
+  source      = "terraform-az-modules/resource-group/azurerm"
+  version     = "1.0.3"
+  name        = "core"
+  environment = "dev"
+  location    = "centralus"
+  label_order = ["name", "environment", "location"]
+}
+
+##-----------------------------------------------------------------------------
+## Virtual Network
+##-----------------------------------------------------------------------------
+module "vnet" {
+  source              = "terraform-az-modules/vnet/azurerm"
+  version             = "1.0.3"
+  name                = "core"
+  environment         = "dev"
+  label_order         = ["name", "environment", "location"]
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_spaces      = ["10.0.0.0/16"]
+}
+
+##-----------------------------------------------------------------------------
+## Subnet
+##-----------------------------------------------------------------------------
+module "subnet" {
+  source               = "terraform-az-modules/subnet/azurerm"
+  version              = "1.0.1"
+  environment          = "dev"
+  label_order          = ["name", "environment", "location"]
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = module.vnet.vnet_name
+  subnets = [
+    {
+      name            = "subnet1"
+      subnet_prefixes = ["10.0.1.0/24"]
+    }
+  ]
+}
+
+##-----------------------------------------------------------------------------
+## Log Analytics Workspace
+##-----------------------------------------------------------------------------
+module "log-analytics" {
+  source                      = "terraform-az-modules/log-analytics/azurerm"
+  version                     = "1.0.2"
+  name                        = "core"
+  environment                 = "dev"
+  label_order                 = ["name", "environment", "location"]
+  log_analytics_workspace_sku = "PerGB2018"
+  resource_group_name         = module.resource_group.resource_group_name
+  location                    = module.resource_group.resource_group_location
+  log_analytics_workspace_id  = module.log-analytics.workspace_id
+}
+
+##-----------------------------------------------------------------------------
+## Private DNS Zone
+##-----------------------------------------------------------------------------
+module "private_dns_zone" {
+  source              = "terraform-az-modules/private-dns/azurerm"
+  version             = "1.0.2"
+  location            = module.resource_group.resource_group_location
+  name                = "dns"
+  environment         = "dev"
+  resource_group_name = module.resource_group.resource_group_name
+  label_order         = ["name", "environment", "location"]
+  private_dns_config = [
+    {
+      resource_type = "key_vault"
+      vnet_ids      = [module.vnet.vnet_id]
+    }
+  ]
+}
+
+##-----------------------------------------------------------------------------
+## Key Vault
+##-----------------------------------------------------------------------------
+module "vault" {
+  source                        = "terraform-az-modules/key-vault/azurerm"
+  version                       = "1.0.1"
+  name                          = "core"
+  environment                   = "dev"
+  label_order                   = ["name", "environment", "location"]
+  resource_group_name           = module.resource_group.resource_group_name
+  location                      = module.resource_group.resource_group_location
+  subnet_id                     = module.subnet.subnet_ids.subnet1
+  public_network_access_enabled = true
+  sku_name                      = "premium"
+  private_dns_zone_ids          = module.private_dns_zone.private_dns_zone_ids.key_vault
+  network_acls = {
+    bypass         = "AzureServices"
+    default_action = "Deny"
+    ip_rules       = ["0.0.0.0/0"]
+  }
+  reader_objects_ids = {
+    "Key Vault Administrator" = {
+      role_definition_name = "Key Vault Administrator"
+      principal_id         = data.azurerm_client_config.current_client_config.object_id
+    }
+  }
+  diagnostic_setting_enable  = true
+  log_analytics_workspace_id = module.log-analytics.workspace_id
+}
+
+##-----------------------------------------------------------------------------
+## Azure Kubernetes Service (AKS)
+##-----------------------------------------------------------------------------
+module "aks" {
+  source                = "../../"
+  name                  = "core"
+  environment           = "dev"
+  resource_group_name   = module.resource_group.resource_group_name
+  location              = module.resource_group.resource_group_location
+  private_dns_zone_id   = module.private_dns_zone.private_dns_zone_ids.azure_kubernetes
+  private_dns_zone_type = "Custom"
+  vnet_id               = module.vnet.vnet_id
+  default_node_pool_config = {
+    name                         = "agentpool"
+    node_count                   = 1
+    vm_size                      = "Standard_D2s_v3"
+    os_type                      = "Linux"
+    enable_auto_scaling          = true
+    enable_host_encryption       = true
+    type                         = "VirtualMachineScaleSets"
+    vnet_subnet_id               = module.subnet.subnet_ids.subnet1
+    max_pods                     = 50
+    os_disk_type                 = "Managed"
+    os_disk_size_gb              = 30
+    enable_node_public_ip        = false
+    mode                         = "System"
+    only_critical_addons_enabled = true
+  }
+
+  node_pools = {
+    user = {
+      vnet_subnet_id         = module.subnet.subnet_ids.subnet1
+      vm_size                = "Standard_D2s_v3"
+      enable_auto_scaling    = true
+      enable_host_encryption = true
+      max_pods               = 50
+    }
+  }
+
+  key_vault_id               = module.vault.id
+  admin_objects_ids          = [data.azurerm_client_config.current_client_config.object_id]
+  microsoft_defender_enabled = false
+  diagnostic_setting_enable  = false
+  log_analytics_workspace_id = module.log-analytics.workspace_id
+}
