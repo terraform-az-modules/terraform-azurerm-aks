b000aaf71fc8e118a8f9215fc237c97e717b5e93
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 18:50:17 +0100

    feat: add wrapper module for multi-instance Terragrunt support (#123)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

---
+++ wrappers/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.10.0"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.31.0"
+    }
+  }
+}
